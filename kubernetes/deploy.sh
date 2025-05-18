c77e71a6b787dfb68c4d06287700b4a811fba25e
afrah3b <afrahbawhab@gmail.com>
2025-05-18 13:42:03 +0300

    url update, kubernetes pipeline with monitoring

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -7,3 +7,9 @@ kubectl apply -f frontend-deploy.yml
 kubectl apply -f config_map.yml
 kubectl apply -f frontend_cluster_ip.yml
 kubectl apply -f frontend_ingress.yml
+
+kubectl apply -f monitoring-namespace.yml
+kubectl apply -f grafana-deploy.yml
+kubectl apply -f grafana-service.yml
+kubectl apply -f prometheus-deploy.yml
+kubectl apply -f prometheus-service.yml
