567274f23bc9b16ec55b1599836cf42d63a8e295
afrah3b <afrahbawhab@gmail.com>
2025-05-18 14:06:37 +0300

    home with techmomentum title

--- authentication_app_frontend/components/Home.tsx
+++ authentication_app_frontend/components/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
 
       <h1 className="text-6xl font-semibold drop-shadow-lg mb-3">User Authentication 🔐</h1>
       <div className="text-2xl drop-shadow-lg">Authentication for any of your<span className="bg-green-500/50 font-semibold px-2 py-1 drop-shadow-xl rounded-lg">Mobile</span> or <span className="bg-purple-500/50 font-semibold px-2 py-1 drop-shadow-xl rounded-lg">Web App</span></div>
+      <div className="text-2xl drop-shadow-lg">TechMomentum Team</div>
 
     </div>
   )
